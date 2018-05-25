ac6d1f5f09ea6c89b9bd89bbc3762b08b62a9343
Christian Frauenstein <cfrauenstein@gmail.com>
2018-05-25 12:19:49 +0200

    added a pixi vibe

--- src/components/screens/PixiJSCanvasContainer.js
+++ src/components/screens/PixiJSCanvasContainer.js
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 import { assetManager } from '../../singletons/AssetManager'
 import * as dat from 'dat.gui';
 
+import * as PIXI from 'pixi.js'
+
+
 const ContainerDiv = styled.div`
 padding: 20px;
 bottom:0;
@@ -28,164 +31,205 @@ border-style: solid;
 border-width: 5px;
 border-color: #232323;
 `
-const guiAPI = {
-
-};
 
 
 
-class ThreeJSCanvasContainer extends Component {
-  state = {
-
-  }
-
-  constructor() {
-    super();
-
-  }
-
-  componentDidMount() {
+class PixiJSCanvasContainer extends Component {
 
 
-    this.onResize();
-    window.addEventListener("resize", this.onResize.bind(this));
-	  this.startRequestAnimationFrame();
+	state = {
+
+	}
+
+	constructor() {
+		super();
+
+		this.guiState = {
+			cameraZoomLevel: 1,
+			cameraPanX: 0,
+			cameraPanY: 0,
+		}
+
+	}
+
+	componentDidMount() {
 
 
-    // Create a scene
-    this.setState({
+		this.onResize();
+		window.addEventListener("resize", this.onResize.bind(this));
+		this.startRequestAnimationFrame();
+
+		let tilemapCanvas = document.createElement('canvas');
+		tilemapCanvas.width = this.props.tilemapJSON.width * this.props.tilemapJSON.tilewidth;
+		tilemapCanvas.height = this.props.tilemapJSON.height * this.props.tilemapJSON.tileheight;
+
+		console.log("tilemapCanvas: ", tilemapCanvas);
+		// Create a scene
+		this.setState({
+			pixiApp: new PIXI.Application({ view: this.canvasElement, width: this.canvasElement.width, height: this.canvasElement.height }),
+			tilemapCanvas: tilemapCanvas,
+			tilemapCanvasContext: tilemapCanvas.getContext('2d')
+		}, ()=>{
+
+			// PIXI.BaseTexture.SCALE_MODE.DEFAULT = PIXI.BaseTexture.SCALE_MODE.NEAREST;
+
+			console.log(PIXI.SCALE_MODES);
+			this.updateTilemapCanvas(this.props);
+
+			let texture = PIXI.Texture.fromCanvas(this.state.tilemapCanvas, PIXI.SCALE_MODES.NEAREST);
+			let sprite1 = new PIXI.Sprite(texture);
+			sprite1.position.set(-sprite1.width/2, -sprite1.height/2)
+			// let graphic = new PIXI.Graphics();
+			// graphic.beginFill(0x5cafe2);
+			// graphic.drawRect(10,10,400,400);
+
+			this.camera = new PIXI.Container();
+			this.camera.addChild(sprite1);
+			this.state.pixiApp.stage.addChild(this.camera);
+
+			this.setGuiState();
 
 
-    }, ()=>{
 
 			this.gui = new dat.GUI();
 			this.initGui();
-    });
+		});
 
-  }
+	}
+
+	updateTilemapCanvas(props) {
+
+		this.state.tilemapCanvasContext.resetTransform();
+		this.state.tilemapCanvasContext.clearRect(0,0, this.state.tilemapCanvas.width, this.state.tilemapCanvas.height);
+
+		let spritesheet = assetManager.getTilesetImage();
+		for (let [index, tileId] of props.tilemapJSON.layers[0].data.entries()){
+
+			this.drawTile(this.state.tilemapCanvasContext, spritesheet, {
+				xPositionInSpritesheet: ((tileId % (props.tilesetJSON.imagewidth/props.tilesetJSON.tilewidth)) - 1)  * props.tilesetJSON.tilewidth,
+				yPositionInSpritesheet: (Math.floor(tileId / (props.tilesetJSON.imagewidth/props.tilesetJSON.tileheight))) * props.tilesetJSON.tilewidth,
+				widthOfTileInSpritesheet: props.tilesetJSON.tilewidth,
+				heightOfTileInSpritesheet: props.tilesetJSON.tileheight,
+				xPositionOnCanvas: (index % props.tilemapJSON.layers[0].width) * props.tilesetJSON.tilewidth,
+				yPositionOnCanvas: (Math.floor(index / props.tilemapJSON.layers[0].width)) * props.tilesetJSON.tilewidth,
+				widthOfTileOnCanvas: props.tilesetJSON.tilewidth,
+				heightOfTileOnCanvas: props.tilesetJSON.tileheight
+			})
+		}
+
+	}
+
+	drawTile(ctx, spritesheet, drawDetails){
+		ctx.drawImage(spritesheet, drawDetails.xPositionInSpritesheet, drawDetails.yPositionInSpritesheet, drawDetails.widthOfTileInSpritesheet, drawDetails.heightOfTileInSpritesheet, drawDetails.xPositionOnCanvas, drawDetails.yPositionOnCanvas, drawDetails.widthOfTileOnCanvas, drawDetails.heightOfTileOnCanvas);
+	}
 
 
+	initGui = () => {
+		var drop;
+
+		this.gui.add( this.guiState, 'cameraZoomLevel', 0.1, 6.0 ).name( 'zoom' ).onChange(this.setGuiState);
+		this.gui.add( this.guiState, 'cameraPanX', -1000, 1000 ).name( 'panX' ).onChange(this.setGuiState);
+		this.gui.add( this.guiState, 'cameraPanY', -1000, 1000 ).name( 'panY' ).onChange(this.setGuiState);
+
+	}
+
+	setGuiState = () =>{
+		this.camera.scale.set(this.guiState.cameraZoomLevel, this.guiState.cameraZoomLevel);
+		this.camera.pivot.set(this.guiState.cameraPanX, this.guiState.cameraPanY)
+		this.camera.position.set(this.canvasElement.width/2, this.canvasElement.height/2);
+	}
 
 
-  initGui = () => {
-  	var drop;
-
-  	// this.gui.add( guiAPI, 'offsetX', 0.0, 1.0 ).name( 'offset.x' ).onChange( this.updateUvTransform );
-  	// this.gui.add( guiAPI, 'offsetY', 0.0, 1.0 ).name( 'offset.y' ).onChange( this.updateUvTransform );
-  	// this.gui.add( guiAPI, 'repeatX', 0.0, 2.0 ).name( 'repeat.x' ).onChange( this.updateUvTransform );
-  	// this.gui.add( guiAPI, 'repeatY', 0.0, 2.0 ).name( 'repeat.y' ).onChange( this.updateUvTransform );
-  	// this.gui.add( guiAPI, 'rotation', - 2.0, 2.0 ).name( 'rotation' ).onChange( this.updateUvTransform );
-  	// this.gui.add( guiAPI, 'centerX', 0.0, 1.0 ).name( 'center.x' ).onChange( this.updateUvTransform );
-  	// this.gui.add( guiAPI, 'centerY', 0.0, 1.0 ).name( 'center.y' ).onChange( this.updateUvTransform );
-  }
-
-  updateUvTransform = () => {
-  	// var texture = this.mesh.material.map;
-  	// if ( texture.matrixAutoUpdate === true ) {
-  	// 	texture.offset.set( guiAPI.offsetX, guiAPI.offsetY );
-  	// 	texture.repeat.set( guiAPI.repeatX, guiAPI.repeatY );
-  	// 	texture.center.set( guiAPI.centerX, guiAPI.centerY );
-  	// 	texture.rotation = guiAPI.rotation; // rotation is around [ 0.5, 0.5 ]
-  	// } else {
-  	// 	// one way...
-  	// 	//texture.matrix.setUvTransform( guiAPI.offsetX, guiAPI.offsetY, guiAPI.repeatX, guiAPI.repeatY, guiAPI.rotation, guiAPI.centerX, guiAPI.centerY );
-  	// 	// another way...
-  	// 	texture.matrix
-  	// 	    .identity()
-  	// 	    .translate( - guiAPI.centerX, - guiAPI.centerY )
-  	// 	    .rotate( guiAPI.rotation )					// I don't understand how rotation can preceed scale, but it seems to be required...
-  	// 	    .scale( guiAPI.repeatX, guiAPI.repeatY )
-  	// 	    .translate( guiAPI.centerX, guiAPI.centerY )
-  	// 	    .translate( guiAPI.offsetX, guiAPI.offsetY );
-  	// }
-  }
-
-  componentWillUnmount() {
+	componentWillUnmount() {
 
 		this.gui.destroy()
-    window.removeEventListener("resize", this.onResize.bind(this));
+		window.removeEventListener("resize", this.onResize.bind(this));
 
-  }
-
-
-  startRequestAnimationFrame() {
-    if (!this.requestId) {
-      this.requestId = window.requestAnimationFrame(this.animationLoop);
-    }
-  }
-
-  stopRequestAnimationFrame() {
-    if (this.requestId) {
-      window.cancelAnimationFrame(this.requestId);
-      this.requestId = undefined;
-    }
-  }
-
-  animationLoop = (time) => {
-    this.requestId = undefined;
+	}
 
 
-    this.startRequestAnimationFrame();
-  }
+	startRequestAnimationFrame() {
+		if (!this.requestId) {
+			this.requestId = window.requestAnimationFrame(this.animationLoop);
+		}
+	}
 
-  onResize() {
-    if (this.canvasElement){
-      this.updateCanvasDimensions();
-    }
-  }
+	stopRequestAnimationFrame() {
+		if (this.requestId) {
+			window.cancelAnimationFrame(this.requestId);
+			this.requestId = undefined;
+		}
+	}
 
-  updateCanvasDimensions() {
-    if (this.canvasElement){
-
-      //Make fullscreen canvas, but we can do anything here.
-
-      // Lookup the size the browser is displaying the canvas.
-      var displayWidth  = 800;
-      var displayHeight = 800;
-
-      // Check if the canvas is not the same size.
-      if (this.canvasElement.width  !== 800 || this.canvasElement.height !== 800) {
-
-        // Make the canvas the same size
-        this.canvasElement.width  = displayWidth;
-        this.canvasElement.height = displayHeight;
+	animationLoop = (time) => {
+		this.requestId = undefined;
 
 
-      }
-    }
-  }
+		this.startRequestAnimationFrame();
+	}
 
-  screenTap = () => {
-  }
+	onResize() {
+		if (this.canvasElement){
+			this.updateCanvasDimensions();
+		}
+	}
+
+	updateCanvasDimensions() {
+		if (this.canvasElement){
+
+
+			//Make fullscreen canvas, but we can do anything here.
+
+			// Lookup the size the browser is displaying the canvas.
+			var displayWidth  = 800;
+			var displayHeight = 800;
+
+			// Check if the canvas is not the same size.
+			if (this.canvasElement.width  !== 800 || this.canvasElement.height !== 800) {
+
+				// Make the canvas the same size
+				console.log("displayWidth: ", displayWidth);
+				this.canvasElement.width  = displayWidth;
+				this.canvasElement.height = displayHeight;
+
+
+			}
+		}
+	}
+
+	screenTap = () => {
+	}
 
 	onWheel = (e) => {
 		console.log('hallo')
 	}
 
 
-  shouldComponentUpdate(props) {
-    return false;
-  }
+	shouldComponentUpdate(props) {
 
-  render() {
-    return (
-      <ContainerDiv>
-        <StyledHeading>Canvas:</StyledHeading>
-        <StyledCanvas innerRef={(canvas) => { this.canvasElement = canvas }} onClick={() => this.screenTap() } />
-      </ContainerDiv>
-    );
-  }
+
+		return false;
+	}
+
+	render() {
+		return (
+			<ContainerDiv>
+				<StyledHeading>Canvas:</StyledHeading>
+				<StyledCanvas innerRef={(canvas) => { this.canvasElement = canvas }} onClick={() => this.screenTap() } />
+			</ContainerDiv>
+		);
+	}
 }
 
 
 export default connect((state) =>{
-  return {
-    assets: state.assetState,
-    tilesetJSON: state.assetState.tilesetJSON,
-    tilemapJSON: state.assetState.tilemapJSON
-  }
+	return {
+		assets: state.assetState,
+		tilesetJSON: state.assetState.tilesetJSON,
+		tilemapJSON: state.assetState.tilemapJSON
+	}
 },
 {
 
 }
-)(ThreeJSCanvasContainer);
+)(PixiJSCanvasContainer);
