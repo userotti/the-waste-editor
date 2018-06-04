716762e900b29bbdd2c6c89662632e4eb2660e1d
Christian Frauenstein <cfrauenstein@gmail.com>
2018-06-04 08:03:28 +0200

    playing with the tile maps

--- public/static/assets/custom/base_tile_set.tsx
+++ public/static/assets/custom/base_tile_set.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="thewaste" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+<tileset name="base_tile_set2" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="exports/base_tile_sheet.png" trans="ff00ff" width="512" height="512"/>
 </tileset>
